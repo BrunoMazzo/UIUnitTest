3cbdc7cde2546e86fd2f86cd9e66d8f1b91924e5
Bruno Mazzo <mazzo.bruno@gmail.com>
2023-03-16 16:37:03 +1100

    Remove unused ElementMatcher

--- Lib/Sources/UIUnitTest/Actions/Query+Tap.swift
+++ Lib/Sources/UIUnitTest/Actions/Query+Tap.swift
@@ -6,22 +6,3 @@
 //
 
 import Foundation
-
-public struct TapRequest: Codable {
-    
-    public var matchers: [ElementMatcher]
-    public var duration: TimeInterval?
-    public var numberOfTaps: Int?
-    public var numberOfTouches: Int?
-
-    public init(matchers: [ElementMatcher], duration: TimeInterval? = nil) {
-        self.matchers = matchers
-        self.duration = duration
-    }
-    
-    public init(matchers: [ElementMatcher], numberOfTaps: Int? = nil, numberOfTouches: Int? = nil) {
-        self.matchers = matchers
-        self.numberOfTaps = numberOfTaps
-        self.numberOfTouches = numberOfTouches
-    }
-}
