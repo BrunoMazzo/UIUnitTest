20d52bbd7c506591f439b40a7c69ec5e47307e05
Bruno Mazzo <mazzo.bruno@gmail.com>
2023-05-25 19:20:06 +1000

    Make CI run just basic tests

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -6,20 +6,21 @@ rm -rf cov.json
 rm -rf lcov.info
 
 # Clean server because Xcode its getting confused with the packages
-xcodebuild -workspace UIUnitTest.xcworkspace -scheme Server clean
+# xcodebuild -workspace UIUnitTest.xcworkspace -scheme Server clean
 
-serverCommand="xcodebuild -workspace UIUnitTest.xcworkspace -scheme ServerUITests test -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' -enableCodeCoverage YES -derivedDataPath build -resultBundlePath build/Server.xcresult -clonedSourcePackagesDirPath SourcePackages/server"
-clientCommand="xcodebuild -workspace UIUnitTest.xcworkspace -scheme Client test -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' -enableCodeCoverage YES -derivedDataPath build -resultBundlePath build/Client.xcresult -clonedSourcePackagesDirPath SourcePackages/client"
+# serverCommand="xcodebuild -workspace UIUnitTest.xcworkspace -scheme ServerUITests test -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' -enableCodeCoverage YES -derivedDataPath build -resultBundlePath build/Server.xcresult -clonedSourcePackagesDirPath SourcePackages/server"
+# clientCommand="
+xcodebuild -workspace UIUnitTest.xcworkspace -scheme Client test -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' -derivedDataPath build -resultBundlePath build/Client.xcresult -clonedSourcePackagesDirPath SourcePackages/client
 
 
 # eval "sleep 20 && $clientCommand"
 
-eval $serverCommand  & eval "sleep 20 && $clientCommand" & wait
+# eval $serverCommand  & eval "sleep 20 && $clientCommand" & wait
 
-xcrun xcresulttool merge build/Client.xcresult build/Server.xcresult --output-path merged.xcresult
+# xcrun xcresulttool merge build/Client.xcresult build/Server.xcresult --output-path merged.xcresult
 
-xcrun xccov view --report --json merged.xcresult > cov.json
+# xcrun xccov view --report --json merged.xcresult > cov.json
 
-swift run xccov2lcov cov.json > lcov.info
+# swift run xccov2lcov cov.json > lcov.info
 
 # xcrun xccov view merged.xcresult --report --only-targets
