96c44b26e3bda88e33651c753559bcb39d429785
Bruno Mazzo <mazzo.bruno@gmail.com>
2023-08-26 12:53:54 +1000

    Start changing install command to support clone simulators

--- Lib/Sources/UIUnitTestCLI/InstallCommand.swift
+++ Lib/Sources/UIUnitTestCLI/InstallCommand.swift
@@ -2,28 +2,57 @@ import ArgumentParser
 import Foundation
 
 
+@available(macCatalyst 16.0, *)
 struct InstallCommand: AsyncParsableCommand {
     @Flag(help: "Reinstall the server even if the server already installed")
     var forceInstall = false
+    
+//  DEVICE_NAME
+    var deviceName: String = "iPhone 14"
+    
+//  TARGET_DEVICE_OS_VERSION
+    var osVersion: String = "16.2"
 
     mutating func run() async throws {
-        let result: String = await executeShellCommand("xcrun simctl listapps booted")
+        let cloneDeviceLists: String = await executeShellCommand("xcrun simctl --set testing list")
         
-        let appInstalled = result.contains("bruno.mazzo.ServerUITests.xctrunner")
+        let versionsRegex = try! Regex("-- iOS \(osVersion.replacingOccurrences(of: ".", with: "\\.")) --\\n([\\n\\sA-Za-z\\d\\(\\)-]*)\\n--")
         
-        if !appInstalled || forceInstall {
-            let serverRunnerZip = Bundle.module.url(forResource: "Server", withExtension: ".zip")!
-            
-            let tempDirectory = getTempFolder()
-            
-            let testRunnerZip = copyFile(file: serverRunnerZip, toFolder: tempDirectory)
-            
-            await executeShellCommand("unzip -o \(testRunnerZip.path) -d \(tempDirectory.relativePath)")
-            
-            let rootFolder = String(tempDirectory.pathComponents.joined(separator: "/").dropFirst())
-            
-            await executeShellCommand("xcrun simctl install booted \(rootFolder)/ServerUITests-Runner.app")
+        guard let devices = try? versionsRegex.firstMatch(in: cloneDeviceLists)?.output[0] else {
+            print("Error regex")
+            return
         }
+        
+        let allDevices = String(cloneDeviceLists[devices.range!])
+        
+        print(allDevices)
+        
+        let deviceRegex = try! Regex("Clone [0-9]* of \(deviceName) \\(([0-9A-F-]*)\\) \\(Booted\\)")
+        for match in allDevices.matches(of: deviceRegex) {
+            let deviceIdentifier = String(allDevices[match.output[1].range!])
+            
+            let result: String = await executeShellCommand("xcrun simctl --set testing listapps \(deviceIdentifier)")
+            
+            let appInstalled = result.contains("bruno.mazzo.ServerUITests.xctrunner")
+            
+            if !appInstalled || forceInstall {
+                let serverRunnerZip = Bundle.module.url(forResource: "PreBuild", withExtension: ".zip")!
+                
+                let tempDirectory = getTempFolder()
+                
+                let testRunnerZip = copyFile(file: serverRunnerZip, toFolder: tempDirectory)
+                
+                await executeShellCommand("unzip -o \(testRunnerZip.path) -d \(tempDirectory.relativePath)")
+                
+                let rootFolder = String(tempDirectory.pathComponents.joined(separator: "/").dropFirst())
+                
+                await executeShellCommand("xcrun simctl --set testing install \(deviceIdentifier) \(rootFolder)/ServerUITests-Runner.app")
+            }
+            
+            await executeShellCommand("xcrun simctl --set testing launch \(deviceIdentifier) bruno.mazzo.ServerUITests.xctrunner")
+        }
+        
+        
     }
     
     func getTempFolder() -> URL {
