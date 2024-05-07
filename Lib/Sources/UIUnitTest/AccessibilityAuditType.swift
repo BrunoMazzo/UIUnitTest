0db796a7dd666d64d67151cd80b41f49329418f6
Bruno Mazzo <mazzo.bruno@gmail.com>
2024-05-07 20:37:42 +1000

    Fix accessibility audit type names

--- Lib/Sources/UIUnitTest/AccessibilityAuditType.swift
+++ Lib/Sources/UIUnitTest/AccessibilityAuditType.swift
@@ -15,23 +15,23 @@ public struct AccessibilityAuditType: RawRepresentable, OptionSet {
         self.rawValue = rawValue
     }
     
-    public static let Contrast                     = AccessibilityAuditType(rawValue: 1 << 0)
-    public static let ElementDetection             = AccessibilityAuditType(rawValue: 1 << 1)
-    public static let HitRegion                    = AccessibilityAuditType(rawValue: 1 << 2)
-    public static let SufficientElementDescription = AccessibilityAuditType(rawValue: 1 << 3)
+    public static let contrast                     = AccessibilityAuditType(rawValue: 1 << 0)
+    public static let elementDetection             = AccessibilityAuditType(rawValue: 1 << 1)
+    public static let hitRegion                    = AccessibilityAuditType(rawValue: 1 << 2)
+    public static let sufficientElementDescription = AccessibilityAuditType(rawValue: 1 << 3)
     
 #if TARGET_OS_IOS || TARGET_OS_TV || TARGET_OS_WATCH || TARGET_OS_SIMULATOR
     // Types of audits supported on iOS, watchOS, and tvOS
-    public static let DynamicType                  = AccessibilityAuditType(rawValue: 1 << 16)
-    public static let TextClipped                  = AccessibilityAuditType(rawValue: 1 << 17)
-    public static let Trait                        = AccessibilityAuditType(rawValue: 1 << 18)
+    public static let dynamicType                  = AccessibilityAuditType(rawValue: 1 << 16)
+    public static let textClipped                  = AccessibilityAuditType(rawValue: 1 << 17)
+    public static let trait                        = AccessibilityAuditType(rawValue: 1 << 18)
     
 #elseif TARGET_OS_OSX || TARGET_OS_MACCATALYST
     // Types of audits supported on macOS
-    public static let Action                       = AccessibilityAuditType(rawValue: 1 << 32)
-    public static let ParentChild                  = AccessibilityAuditType(rawValue: 1 << 33)
+    public static let action                       = AccessibilityAuditType(rawValue: 1 << 32)
+    public static let parentChild                  = AccessibilityAuditType(rawValue: 1 << 33)
 #endif
-    public static let All                          = AccessibilityAuditType(rawValue: ~0)
+    public static let all                          = AccessibilityAuditType(rawValue: ~0)
 }
 
 public class AccessibilityAuditIssue {
