6caa8eb91d759339fc3b907e8101c70b1a770e26
Bruno Mazzo <mazzo.bruno@gmail.com>
2024-05-18 12:00:41 +1000

    Revert CI Cache

--- stop-server.sh
+++ stop-server.sh
@@ -5,7 +5,5 @@ unset SDKROOT
 mkdir -p "${PROJECT_DIR}/.uiUnitTest/Logs"
 exec > "${PROJECT_DIR}/.uiUnitTest/Logs/stop-logs.txt" 2>&1
 
-UIUnitTestPath=$(dirname $0)
-
 # Stop any process still running
-swift run --package-path "$UIUnitTestPath" UIUnitTestCLI stop-command
+swift run --package-path "$BUILD_DIR/../../SourcePackages/checkouts/UIUnitTest" UIUnitTestCLI stop-command
