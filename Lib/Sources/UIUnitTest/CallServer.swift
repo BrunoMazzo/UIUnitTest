ff7348cf036ff5fe33cbaae4bc2b6aa211f3705d
Bruno Mazzo <mazzo.bruno@gmail.com>
2024-06-21 13:32:10 +1000

    Try to make sync request for the sync API

--- Lib/Sources/UIUnitTest/CallServer.swift
+++ Lib/Sources/UIUnitTest/CallServer.swift
@@ -45,6 +45,55 @@ final class ServerAPI: Sendable {
             throw NSError(domain: "Test", code: 1, userInfo: ["reason": error])
         }
     }
+    
+    @available(*, noasync)
+    func callServer<RequestData: Codable & Sendable, ResponseData: Codable & Sendable>(
+        path: String,
+        request: RequestData
+    ) throws -> ResponseData {
+        let encoder = JSONEncoder()
+        
+        let activateUrl = URL(string: "http://localhost:\(port)/\(path)")!
+        var activateRequest = URLRequest(url: activateUrl)
+        activateRequest.httpMethod = "POST"
+        activateRequest.httpBody = try encoder.encode(request)
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let result: SendableBox<UIResponse<ResponseData>> = SendableBox(value: nil)
+        let dataTask = URLSession.shared.dataTask(with: activateRequest) { data,_,error in
+            if let error {
+                result.value = UIResponse<ResponseData>(error: error.localizedDescription)
+            } else {
+                do {
+                    let decoder = JSONDecoder()
+                    result.value = try decoder.decode(UIResponse<ResponseData>.self, from: data!)
+                } catch {
+                    result.value = UIResponse<ResponseData>(error: error.localizedDescription)
+                }
+            }
+            semaphore.signal()
+        }
+        
+        dataTask.resume()
+        semaphore.wait()
+        
+        switch result.value!.response {
+        case .success(data: let response):
+            return response
+        case .error(error: let error):
+            print(error.error)
+            throw NSError(domain: "Test", code: 1, userInfo: ["reason": error])
+        }
+    }
+}
+
+final class SendableBox<T: Sendable>: @unchecked Sendable {
+    public var value: T?
+    
+    init(value: T? = nil) {
+        self.value = value
+    }
 }
 
 func callServer<RequestData: Codable, ResponseData: Codable>(
@@ -52,6 +101,13 @@ func callServer<RequestData: Codable, ResponseData: Codable>(
     request: RequestData
 ) async throws -> ResponseData {
     try await ServerAPI.shared.callServer(path: path, request: request)
+}
+
+func callServer<RequestData: Codable & Sendable, ResponseData: Codable & Sendable>(
+    path: String,
+    request: RequestData
+) -> ResponseData {
+    try! ServerAPI.shared.callServer(path: path, request: request)
 }
 
 @MainActor
@@ -75,7 +131,11 @@ public enum Response<T: Codable> {
     case success(data: T)
 }
 
-public struct ErrorResponse: Codable {
+extension Response: Sendable where T: Sendable {
+    
+}
+
+public struct ErrorResponse: Codable, Sendable {
     var error: String
 }
 
@@ -118,5 +178,9 @@ public struct UIResponse<T: Codable>: Codable {
             try container.encode(data, forKey: .data)
         }
     }
+    
+}
+
+extension UIResponse: Sendable where T: Sendable {
     
 }
